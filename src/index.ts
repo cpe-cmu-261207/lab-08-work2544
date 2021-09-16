1b0d7dd3b7e70e438c0d027b9fc2d039061887c4
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2021-09-16 14:24:04 +0700

    instyarn/me

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,9 @@ type Task = {
 }
 
 const tasks: Task[] = []
-
+app.get('/me',(req,res)=>{
+  return res.json({name:'latthaphol laohapiboonrattana',code:'630610759'})
+})
 app.get('/todo', (req, res) => {
 
   // try to call /todo?q1=data1&q2data2
